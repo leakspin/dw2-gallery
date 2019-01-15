6d52bfd02c4817b88f6bf951474a38025877fac3
Adrián Mora <amora@adrianmora.me>
2019-01-16 00:08:30 +0100

    image optimization

--- generate_thumbnail.sh
+++ generate_thumbnail.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-for a in "$@"
+for a in images/raw/*
 do
-    echo "$a"
 
     filename=$(basename -- "$a")
+    extension="${filename##*.}"
 
-    ffmpeg -i "$a" -vf scale=500:-1 images/thumbs/"$filename"
+    if [ ! -f "images/thumbs/$filename" -a ! -f "images/web/$filename" ]
+    then
+        echo "$a"
 
-    echo $filename
+        ffmpeg -i "$a" -vf scale=500:-1 images/thumbs/"$filename"
+
+        if [[ $extension == 'jpg' ]]
+        then
+            jpegtran -optimize -copy none -perfect -v "$a" > images/web/"$filename"
+        elif [[ $extension == 'png' ]]
+        then
+            optipng -out images/web/"$filename" "$a"
+        else
+            cp "$a" images/web/"$filename"
+        fi
+    fi
 done
